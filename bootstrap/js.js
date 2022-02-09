1172a57ae6df020f30c8679eb5fdb4e6658819d6
TemurUsmonqulov <temur@test.com>
2022-02-09 18:53:31 +0500

    jdjdjdjdjdjd

---
+++ bootstrap/js.js
@@ -0,0 +1,295 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let user = {name: "USMONQULOV TEMUR", phone: "+998915006090", kasp: "Future Web-devoloper", email: "timurusmonqulov"}
+
+// function drawCard(){
+//     let card = document.getElementById('myjs');
+
+//     let cardbody = document.createElement("div")
+//     cardbody.className="card-body"
+   
+//    let div1 = document.createElement("div")
+//    div1.className="div1"
+   
+//    let nameh4 = document.createElement("h4")
+//    nameh4.append("name")
+   
+//    let div2 = document.createElement("div")
+//    div2.className="div2"
+   
+//    let nameh5 = document.createElement("h5")
+//    nameh5.append("kasp")
+   
+//    let namep = document.createElement("p")
+//    namep.append("phone")
+   
+//    let div3 = document.createElement("div")
+//    div3.className="div3 card-body"
+//    let button = document.createElement("button")
+//    button.append("email")
+   
+//    card.append(cardbody)
+//    cardbody.append(div1)
+//    div1.append(nameh4)
+//    card.append(div2)
+//    div2.append(nameh5)
+//    div2.append(namep)
+//    card.append(div3)
+//    div3.append(button)
+//    button.append(button)
+   
+   
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let name1 = "abbdullajon";
+
+// console.log(name1);
+// name1 = "Boshqa";
+// console.log(name1);
+
+
+// const lname = "Qosimov";
+// console.log(lname);
+
+
+// lname = "123"
+
+// data types
+// string, number, boolean, null, undefined, object, array,
+// pasdagilarga tegishli bular
+
+//string
+
+// const houseNumber = 123;
+// const isMarried = false;
+// const age = null;
+// const lname = undefined;
+
+// console.log(typeof lname)
+
+// const streetName = "Pressident";
+// const houseNumber = 157;
+// const tenant = "John Doe";
+
+// console.log(tenant + " " + streetName + " " + houseNumber);
+
+// console.log(tenant.toLocaleUpperCase())
+
+
+// const cars = ["Audi", "BMW", "Tesla"];
+
+// console.log(cars);
+
+// cars.unshift("Toyota");
+
+// console.log(cars);
+
+
+
+// const person = {
+//     name: "Temur",
+//     age: 14,
+//     isStudent: true,
+//     address: {
+//         street: "Main st",
+//         houseNumber: 123
+//     }
+// }
+
+// console.log(person.name, person.address.houseNumber);
+
+// const { name, age, isStudent, address: {street} } = person;
+
+// console.log(name, age, isStudent, street)
+
+// console.log( JSON.stringify(person))
+
+// const students = [
+//   {
+//       fname: "Temur",
+//       age: 23,
+//       gradiated: false
+//   },
+//   {
+//     fname: "Bek",
+//     age: 26,
+//     gradiated: false
+//    }, 
+//    {
+//     fname: "oraz",
+//     age: 24,
+//     gradiated: false
+//    }  
+// ];
+
+// const graduated = students.filter(function(student) {
+//     return student.graduated === true;
+// });
+// console.log(graduated);
+// console.log(students)
+
+
+
+// console.log(students.length);
+// console.log( students[1])
+
+// for (let i=0; i < students.length; i++) {
+//     console.log(i);
+//     console.log(students[i].fname);
+// }
+
+// const x = 29;                 
+// const y = 24;
+
+// if (x<y){
+//     console.log("true");
+// }
+// else{
+//     console.log("folse")
+// }
+
+
+// const score = 62;
+
+// const result = score > 70 ? "A" : "B";
+// console.log(result)
+ 
+
+// function greet(time){
+//     switch(time){
+//         case "morneng":
+//             console.log("Dood aftenun");
+//         break;
+//         case "afternoon":
+//             console.log("good aftrmoon");
+//         break;
+//         case "evening":
+//              console.log("good evening");
+//         break;  
+//     }
+// }
+
+// greet("morneng")
+
+
+
+
+// var numbers = [1, 2, 3, 4, 12, 13, 22, 34]
+// var names = ["ismoil", "jamshid", "sardor", "example"]
+// console.log(numbers[5]);
+// console.log(numbers[2]+ " " +numbers[6]);
+
+// for (let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i]);
+// }
+
+// var star='';
+// for (let i = 0; i < 4; i++) {
+//     for (let j = 0; j < 4; j++) {
+//         if (j==1 || j==2){
+//             star+=" "
+//         }else{
+//             star+="*"
+//         }
+//     }
+//     star+="\n"
+// }
+// console.log(star)
+
+// const students = [
+//     {
+//         fname: "Temur",
+//         age: 25,
+//         gradiated: false
+//     },
+//     {
+//         fname: "Putin",
+//         age: 25,
+//         gradiated: false
+//     },
+//     {
+//         fname: "Bek",
+//         age: 25,
+//         gradiated: false
+//     }
+// ];
+// //console.log(students.length)
+// for (let i = 0; i < students.length; i++){
+//     console.log(i);
+//     console.log(students[i].fname)
+// }
+
+
+
+
+
